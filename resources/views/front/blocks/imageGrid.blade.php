f24d5f7acbc7d00eea2c814b66d06497104ade99
Andrei Ioniță <hi@andrei.io>
2020-06-11 14:18:43 +0100

    Fix imageGrid

--- resources/views/front/blocks/imageGrid.blade.php
+++ resources/views/front/blocks/imageGrid.blade.php
@@ -1,9 +1,30 @@
-<section class="container grid col-gap-8 row-gap-8 md:grid-cols-{{ $block->input('columns') ?? 1 }}">
-    @foreach ($block->imageObjects('image', 'desktop') as $media)
+@php
+    switch ((int) $block->input('columns')) {
+        case 1:
+        default:
+            $cols = 'md:grid-cols-1';
+            break;
+
+        case 2:
+            $cols = 'md:grid-cols-2';
+            break;
+
+        case 3:
+            $cols = 'md:grid-cols-3';
+            break;
+
+        case 4:
+            $cols = 'md:grid-cols-4';
+            break;
+    }
+@endphp
+
+<section class="container grid col-gap-8 row-gap-8 {{ $cols }}">
+    @foreach ($block->imageObjects('image') as $media)
        <x-figure
-            :src="$block->image('image', 'desktop', [], false, false, $media)"
-            :alt="$block->imageAltText('image', $media)"
-            :caption="$block->imageCaption('image', $media)"
+            :src="$block->image('image', 'default', [], false, false, $media)"
+            :alt="$block->imageAltText('image')"
+            :caption="$block->imageCaption('image')"
         />
     @endforeach
 </section>
