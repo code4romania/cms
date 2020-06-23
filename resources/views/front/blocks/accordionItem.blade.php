5ac6d13eeab392970870cab1580661e8ed42bc0c
Andrei Ioniță <hi@andrei.io>
2020-06-23 20:01:00 +0100

    Allow independent control of accordion tabs
    Closes #16

--- resources/views/front/blocks/accordionItem.blade.php
+++ resources/views/front/blocks/accordionItem.blade.php
@@ -2,28 +2,24 @@
     $isPreview = $isPreview ?? false;
 @endphp
 
-<div class="flex flex-col align-center">
+<div class="flex flex-col align-center" x-data="{ open: {{ var_export($index === 0 ?: $isPreview ?: false) }} }">
     <h4
         @if (!$isPreview)
-            x-on:click="selected = (selected !== {{ $index }} ? {{ $index }} : '')"
+            x-on:click="open = !open"
         @endif
         class="flex items-center justify-between px-5 py-3 cursor-pointer"
         :class="{
-            'bg-primary-500 hover:bg-primary-700 text-primary-100' : (selected === {{ $index }}),
-            'bg-gray-300 hover:bg-gray-400 text-black' : (selected !== {{ $index }}),
+            'bg-primary-500 hover:bg-primary-700 text-primary-100' : open,
+            'bg-gray-300 hover:bg-gray-400 text-black' : !open,
         }"
     >
         <span>{{ $block->translatedInput('header') }}</span>
-        <svg class="w-5 h-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path :d="selected === {{ $index }} ? 'M12 8l6 6H6z' : 'M12 16l-6-6h12z'"/>
+        <svg class="w-5 h-5 fill-current" viewBox="0 0 24 24">
+            <path :d="open ? 'M12 8l6 6H6z' : 'M12 16l-6-6h12z'"/>
         </svg>
     </h4>
     <div
-        @if (!$isPreview)
-            x-show="selected === {{ $index }}"
-        @else
-            x-show="true"
-        @endif
+        x-show="{{ !$isPreview ? 'open' : 'true' }}"
         class="px-6 py-4 border rich-text"
     >{!! $block->translatedInput('description') !!}</div>
 </div>
