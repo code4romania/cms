683e98a653a9df9111534bafbb4e43022712b810
Andrei Ioniță <hi@andrei.io>
2020-06-23 22:00:15 +0100

    Add embeds title
    Closes #21

--- resources/views/admin/blocks/embed.blade.php
+++ resources/views/admin/blocks/embed.blade.php
@@ -1,4 +1,10 @@
 @formField('input', [
+    'name'       => 'title',
+    'label'      => __('admin.field.title'),
+    'translated' => true,
+])
+
+@formField('input', [
     'name'         => 'url',
     'label'        => __('admin.field.url'),
     'note'         => __('admin.fieldNote.embedUrl'),
--- resources/views/front/blocks/embed.blade.php
+++ resources/views/front/blocks/embed.blade.php
@@ -1,4 +1,8 @@
 <section class="container">
+    @if ($block->translatedInput('title'))
+        <h1 class="mb-5 h2">{{ $block->translatedInput('title') }}</h1>
+    @endif
+
     <div class="">
         {!! $block->present()->embedCode !!}
     </div>
