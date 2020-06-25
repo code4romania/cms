3a13f237398354f61ddfc881b721a6a90babb4bc
Andrei Ioniță <hi@andrei.io>
2020-06-25 11:19:15 +0100

    Fix missing mailchimp apikey error

--- src/Helpers/MailchimpHelper.php
+++ src/Helpers/MailchimpHelper.php
@@ -7,19 +7,24 @@ namespace Code4Romania\Cms\Helpers;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
 use NZTim\Mailchimp\MailchimpFacade as Mailchimp;
+use Throwable;
 
 class MailchimpHelper
 {
     public static function getListsCached(): Collection
     {
-        return Cache::rememberForever('settings.mailchimp.lists', function () {
-            return collect(Mailchimp::getLists())
-                ->map(function ($list) {
-                    return [
-                        'id'   => $list['id'],
-                        'name' => $list['name'],
-                    ];
-                });
-        });
+        try {
+            return Cache::rememberForever('settings.mailchimp.lists', function () {
+                return collect(Mailchimp::getLists())
+                    ->map(function ($list) {
+                        return [
+                            'id'   => $list['id'],
+                            'name' => $list['name'],
+                        ];
+                    });
+            });
+        } catch (Throwable $th) {
+            return collect();
+        }
     }
 }
