368b8e3b0da7e7f82865725d70f51a8ab63d1fa5
Andrei Ioniță <hi@andrei.io>
2020-11-28 15:11:30 +0000

    feat: 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,18 @@
 @extends('front.layouts.app')
 
 @section('content')
-    404
+    <section class="container text-center">
+        <h1 class="my-10 h2">{{ __('errors.404') }}</h1>
+
+        <div class="my-10">
+            <x-button
+                :href="route('front.pages.index', [], false)"
+                color="primary"
+            >{{ __('errors.back') }}</x-button>
+        </div>
+
+        <div class="w-full max-w-3xl mx-auto my-10">
+            {{ svg('404', 'block w-full h-full') }}
+        </div>
+    </section>
 @endsection
